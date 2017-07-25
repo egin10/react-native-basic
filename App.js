f6c855ff2d509f146450cad578034dbde12ddaa0
egin10 <egin066@gmail.com>
2017-07-25 16:29:38 +0800

    homeComponent

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Home from './components/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Let's Start!</Text>
-      </View>
+      <Home/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Home.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+export default class Home extends Component {
+    state = {
+        alertText: 'KiwKiwKiw',
+        uri: 'https://www.appcoda.com/wp-content/uploads/2015/04/react-native.png'
+      }
+        
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Image 
+            source={{ uri: this.state.uri}} 
+            style={styles.image}
+           />
+
+          <TouchableOpacity onPress={ ()=>{alert(this.state.alertText)} } style={{paddingTop: 10}}>
+            <Text style={styles.textContent}>Let's Start!</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.textFooter}>©2017 egin10</Text>
+        </View>        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    backgroundColor: '#dcdcdc'
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContent: {
+    fontSize: 30,
+    color: 'green'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footer:{
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#a9a9a9'
+  },
+  textFooter: {
+    color: 'white',
+    fontSize: 12
+  }
+})
